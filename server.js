47f0c690ae9d80e2a5e8f543fdf70707bc1d1cf5
Rahul Kanianchalil <rkania3@illinois.edu>
2022-07-07 20:09:50 -0500

    adding resume page, launch templates

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 // server.js
 
 const express = require('express')
+const routes = require('./project/routes/index')
+
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello world from a Node.js app!')
+})
+
+app.get('/resume', (req, res) => {
+    res.sendFile('/project/resume/index.html', { root: '.' })
 })
 
 app.listen(3000, () => {
